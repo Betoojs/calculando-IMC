4a3045d33d717f0fb31c5173d2afb3f62e69a79d
Roberto <robertoleao1212@gmail.com>
2023-08-13 12:31:22 -0300

    correcao de bugs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const pesoUsuario = document.getElementById('inputPeso')
 const alturaUsuario = document.getElementById('inputAltura')
+const exibir = document.getElementById('resultado')
+const exibirNumero = document.getElementById('numerosImc')
 
 function iniciarCalculo() {
     if (pesoUsuario.value === "" && alturaUsuario.value === "") {
@@ -14,27 +16,42 @@ function iniciarCalculo() {
     }
 }
 
-    function exibirResultado() {
+function exibirResultado() {
 
-        let imc = parseFloat(pesoUsuario.value) / (parseFloat(alturaUsuario.value) * parseFloat(alturaUsuario.value))
+    let imc = parseFloat(pesoUsuario.value.replace(",", ".")) / (parseFloat(alturaUsuario.value.replace(",", ".")) * parseFloat(alturaUsuario.value.replace(",", ".")))
 
-        if (imc < 18.5) {
-            alert(`Estado: Magreza seu resultado foi ${imc . toFixed(1)} `)
-        } else if (imc >= 18.5 && imc <= 24.9) {
-            alert(`Estado: Peso normal*seu resultado foi ${imc . toFixed(1)} `)
-        } else if (imc >= 25 && imc <= 29.9) {
-            alert(`Estado: Sobrepeso seu resultado foi ${imc . toFixed(1)} `)
-        } else if (i >= 30 && imc <= 34.9) {
-            alert(`Estado: Obesidade grau I seu resultado foi ${imc . toFixed(1)} `)
-        } else if (imc >= 35 && imc <= 40) {
-            alert(`Estado: Obesidade grau II seu resultado foi ${imc . toFixed(1)} `)
-        } else if (imc > 40) {
-            alert(`Estado: Obesidade grau III seu resultado foi ${imc . toFixed(1)} `)
-        } else {
-            alert('voce digitou um valor invalido')
-        }
-
+    if (imc < 18.5) {
+        exibir.innerHTML = `Estado: magreza seu resultado foi`
+        exibirNumero.innerHTML = imc.toFixed(1)
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        exibir.innerHTML = `Estado: normal seu resultado foi`
+        exibirNumero.innerHTML = imc.toFixed(1)
+    } else if (imc >= 25 && imc <= 29.9) {
+        exibir.innerHTML = `Estado: Sobrepeso seu resultado foi`
+        exibirNumero.innerHTML = imc.toFixed(1)
+    } else if (i >= 30 && imc <= 34.9) {
+        exibir.innerHTML = `Estado: Obesidade grau I seu resultado foi`
+        exibirNumero.innerHTML = imc.toFixed(1)
+    } else if (imc >= 35 && imc <= 40) {
+        exibir.innerHTML = `Estado: Obesidade grau II seu resultado foi`
+        exibirNumero.innerHTML = imc.toFixed(1)
+    } else if (imc > 40) {
+        exibir.innerHTML = `Estado: Obesidade grau III seu resultado foi `
+        exibirNumero.innerHTML = imc.toFixed(1)
+    } else {
+        exibir.innerHTML = 'voce digitou um valor invalido'
     }
+
+}
+
+
+const btnLimpar = document.getElementById('btnLimpar')
+btnLimpar.addEventListener('click', function limpar () {
+    pesoUsuario.value = ""
+    alturaUsuario.value = ""
+    exibir.innerHTML = ""
+    exibirNumero.innerHTML = ""
+})
 
 
 
