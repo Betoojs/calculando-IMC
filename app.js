d890effe4b0406f3719337c21886abb84a808aee
Roberto <robertoleao1212@gmail.com>
2023-08-13 21:29:33 -0300

    concertando bug no peso

--- app.js
+++ app.js
@@ -33,16 +33,16 @@ function exibirResultado() {
     if (imc < 18.5) {
         exibir.innerHTML = `Estado: magreza seu resultado foi`
         exibirNumero.innerHTML = imc.toFixed(1)
-    } else if (imc >= 18.5 && imc <= 24.9) {
+    } else if (imc >= 18.5 && imc < 24.9) {
         exibir.innerHTML = `Estado: normal seu resultado foi`
         exibirNumero.innerHTML = imc.toFixed(1)
-    } else if (imc >= 25 && imc <= 29.9) {
+    } else if (imc >= 24.9 && imc < 29.9) {
         exibir.innerHTML = `Estado: Sobrepeso seu resultado foi`
         exibirNumero.innerHTML = imc.toFixed(1)
-    } else if (i >= 30 && imc <= 34.9) {
+    } else if (imc >= 29.9 && imc < 34.9) {
         exibir.innerHTML = `Estado: Obesidade grau I seu resultado foi`
         exibirNumero.innerHTML = imc.toFixed(1)
-    } else if (imc >= 35 && imc <= 40) {
+    } else if (imc >= 34.9 && imc < 40) {
         exibir.innerHTML = `Estado: Obesidade grau II seu resultado foi`
         exibirNumero.innerHTML = imc.toFixed(1)
     } else if (imc > 40) {
